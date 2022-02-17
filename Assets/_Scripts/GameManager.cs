70f0c0e35c12738dc50a6144a78aaa6a11d73e10
Vincent329 <vincee.ho@gmail.com>
2022-02-17 02:54:58 -0500

    [ADD] - import key model and test game OBJ

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -4,6 +4,35 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+
+    private static GameManager instance;
+
+    public static GameManager Instance
+    {
+        get => instance;
+    }
+
+    [SerializeField]
+    private Camera playerCam;
+    [SerializeField]
+    private Camera lockCam;
+
+    public bool inGame;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
